f064579fb1c8a9931991cf54ca7d46bfbb00802d
Puneeth S <bubblypuneeth@gmail.com>
2026-01-26 17:46:55 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,15 @@
+public class datatypes{
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        char c = 'A';
+        String d = "Hello";
+        boolean e = true;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("String: " + d);
+        System.out.println("Boolean: " + e);
+    }
+}
