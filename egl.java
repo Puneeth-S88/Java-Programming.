d733bc90e7c2010a073dd358957e6884dc069544
Puneeth S <bubblypuneeth@gmail.com>
2026-01-27 21:48:49 +0530

    Add files via upload

---
+++ egl.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class egl{
+    public static void main(String[] args) {
+        Scanner sc=new  Scanner(System.in);
+        System.out.println("Enter the num1:");
+        float num1=sc.nextFloat();
+        System.out.println("Enter the num2:");
+        float num2=sc.nextFloat();
+        if(num1==num2){
+            System.out.println("Both are equal");
+        }
+            else if(num1>num2){
+                System.out.println("num1 is greater");
+            }
+            else{
+                System.out.println("num2 is greater");
+            }
+        }
+    }
+
