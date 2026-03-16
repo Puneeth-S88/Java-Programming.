09e5f646f38ed25cde75931e93fa0e5d42c33ed0
Puneeth S <bubblypuneeth@gmail.com>
2026-03-16 21:53:11 +0530

    Add files via upload

---
+++ diff.java
@@ -0,0 +1,8 @@
+public class diff{
+    public static void main(String[] args) {
+        int a=23;
+        int b=34;
+        int c=a-b;
+        System.out.println("diff is:"+c);
+    }
+}
---
+++ even.java
@@ -0,0 +1,10 @@
+public class even{
+    public static void main(String[] args) {
+        int p=58;
+        if(p%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+        }
+    }
