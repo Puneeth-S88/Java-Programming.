f0ce41d780dd83e24c292b167226eacccee52b61
Puneeth S <bubblypuneeth@gmail.com>
2026-03-16 21:52:35 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,11 @@
+public class largest{
+    public static void main(String[] args) {
+        int a=100;
+        int b=200;
+        if(a>b){
+            System.out.println("largest is:"+a  );
+        }else{
+            System.out.println("largest is:"+b  );
+        }
+    }
+}
