54281b5f17383555c7fd4a632645e3248b9a81e0
Puneeth S <bubblypuneeth@gmail.com>
2026-03-17 22:43:04 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,12 @@
+public class swap {
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("the value of a af swapiing is:"+a);
+        System.out.println("the value of b af swapiing is:"+b);
+    }
+}
