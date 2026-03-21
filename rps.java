26dddad91e83523d9aac4921c2a2b56de9b89c79
Puneeth S <bubblypuneeth@gmail.com>
2026-03-21 19:50:30 +0530

    Update rps.java

--- rps.java
+++ rps.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 public class rps{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ;
-        //System.out.println("Rock=1,Paper=2,sccissor=3");
+        //2
+        // System.out.println("Rock=1,Paper=2,sccissor=3");
         System.out.println("Player 1:\n Enter your choice:");
+
         int num1=sc.nextInt();
         System.out.println("Player 2:\n Enter your choice:");
         int num2 = sc.nextInt();
         if(num1==1 && num2==3 || num1==3 && num2==2 || num1==2 && num2==1){
             System.out.println("Player 1 won");
-        }else{
+        }else if(num1==num2){
+                System.out.println("tie");
+            }
+        
+        else{
             System.out.println("Player 2 won");
         }
        }
