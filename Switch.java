c78fb44d0f90b8804d4d532fab8fd728f3473f77
Puneeth S <bubblypuneeth@gmail.com>
2026-03-24 22:01:02 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,18 @@
+public class Switch {
+    public static void main(String[] args) {
+        String cha = "enid";
+
+        switch (cha) {
+            case "enid":
+                System.out.println("enid is perfect");
+                break;
+
+            case "wed":
+                System.out.println("wed is perfect");
+                break;
+
+            default:
+                System.out.println("it is not perfect");
+        }
+    }
+}
