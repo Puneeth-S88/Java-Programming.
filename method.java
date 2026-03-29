62b9d33849ec7e2e55269b5c03b0e240b9bf69c5
Puneeth S <bubblypuneeth@gmail.com>
2026-03-29 19:58:11 +0530

    Add Demo2 class with addition method

---
+++ method.java
@@ -0,0 +1,11 @@
+class Demo2 {
+    void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+        Demo2 obj = new Demo2();
+        obj.add(10, 20);
+    }
+}
