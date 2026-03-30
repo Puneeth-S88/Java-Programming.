31518c0fe246f7d64bfffe1fa54da491ed85c8f5
Puneeth S <bubblypuneeth@gmail.com>
2026-03-30 20:14:59 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,10 @@
+public class arr {
+    public static void main(String[] args) {
+        int [] arr={99,88,45,23,1,45,0};
+        //int[] arr1;
+        //arr1=arr;
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+}
+}
