c3964a40bd6b6ecd21ab2d2332d00b7a1a8bcf2b
Puneeth S <bubblypuneeth@gmail.com>
2026-03-30 20:15:19 +0530

    Add files via upload

---
+++ arr1.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 9};
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    // swap
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Ascending Order:");
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+
+
+
